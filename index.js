1711bc6cb9663c7a589c2148a063f8de102b73e8
Matheus Rosin <matheus.rosin@gmail.com>
2019-03-22 21:45:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import { useState, useEffect, useRef } from 'react';
 
-export default function useCountdown(seconds) {
+export default function useCountdown(seconds, startRunning = true) {
   if (!Number.isSafeInteger(seconds)) throw Error('Value provided isn\'t valid.');
   
   let secs = Math.abs(seconds);
   const [secondsLeft, setSecondsLeft] = useState(secs);
+  const [running, setRunning] = useState(!!startRunning);
   const countdown = useRef();
   const cleaner = useRef();
 
   useEffect(() => {
-    startCountdown();
+    if (!!startRunning) startCountdown();
     return stopCountdown;
   }, []);
 
@@ -18,17 +19,20 @@ export default function useCountdown(seconds) {
   }
 
   function resumeCountdown() {
+    setRunning(true);
     countdown.current = setInterval(tick, 1000);
     cleaner.current = setTimeout(stopCountdown, secondsLeft * 1000);
   }
   
   function startCountdown() {
     setSecondsLeft(secs);
+    setRunning(true);
     countdown.current = setInterval(tick, 1000);
     cleaner.current = setTimeout(stopCountdown, secs * 1000);
   }
 
   function stopCountdown() {
+    setRunning(false);
     clearInterval(countdown.current);
     clearTimeout(cleaner.current);
   }
@@ -47,6 +51,7 @@ export default function useCountdown(seconds) {
   return {
     countdown: formatted,
     secondsLeft,
+    running,
     startCountdown,
     stopCountdown,
     resumeCountdown,
